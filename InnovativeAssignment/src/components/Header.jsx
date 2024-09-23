20ad571018b06da2c21a67c657296d3bd785368c
ZinDin Zidan <136159336+mzidan132@users.noreply.github.com>
2024-09-23 17:46:54 +0600

    Update Header.jsx

--- InnovativeAssignment/src/components/Header.jsx
+++ InnovativeAssignment/src/components/Header.jsx
@@ -8,28 +8,28 @@ const Header = () => {
 
       <div className="flex space-x-12 ml-20">
         <div className="relative group">
-          <button className="text-white focus:outline-none">
+          <button className="text-white focus:outline-none transition-transform duration-300 hover:scale-300">
             Home 
             <span className="inline-block ml-1 text-sm"><FontAwesomeIcon icon={faChevronDown} /></span>
           </button>
         </div>
 
         <div className="relative group">
-          <button className="text-white focus:outline-none transition-transform duration-300 hover:scale-105">
+          <button className="text-white focus:outline-none transition-transform duration-300 hover:scale-300">
             Event
             <span className="inline-block ml-1"><FontAwesomeIcon icon={faChevronDown} /></span>
           </button>
         </div>
 
         <div className="relative group">
-          <button className="text-white focus:outline-none transition-transform duration-300 hover:scale-105">
+          <button className="text-white focus:outline-none transition-transform duration-300 hover:scale-300">
             Contact
             <span className="inline-block ml-1"><FontAwesomeIcon icon={faChevronDown} /></span>
           </button>
         </div>
 
         <div className="relative group">
-          <button className="text-white focus:outline-none transition-transform duration-300 hover:scale-105">
+          <button className="text-white focus:outline-none transition-transform duration-300 hover:scale-300">
             Blog
             <span className="inline-block ml-1"><FontAwesomeIcon icon={faChevronDown} /></span>
           </button>
