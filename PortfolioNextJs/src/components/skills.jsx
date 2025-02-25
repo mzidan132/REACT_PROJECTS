902e68f426fc6232c2eabd0383e19e34cfe75201
ZinDin Zidan <136159336+mzidan132@users.noreply.github.com>
2025-02-25 11:20:22 +0600

    Update skills.jsx

--- PortfolioNextJs/src/components/skills.jsx
+++ PortfolioNextJs/src/components/skills.jsx
@@ -4,7 +4,7 @@ import {useEffect} from 'react'
 import AOS from 'aos'
 import 'aos/dist/aos.css'
 import { FaHtml5, FaCss3Alt, FaJsSquare, FaReact, FaNodeJs, FaPython, FaGitAlt, FaGithub,FaMountain, FaWordpress } from 'react-icons/fa';
-import { SiMongodb,SiElectron, SiNextdotjs, SiExpress, SiPandas, SiFirebase, SiRender, SiTailwindcss,SiVercel,SiTensorflow, SiBootstrap, SiPhp, SiMysql } from 'react-icons/si';
+import { SiMongodb,SiElectron, SiNextdotjs, SiExpress, SiPandas, SiFirebase, SiRender, SiTailwindcss,SiVercel,SiTensorflow, SiBootstrap, SiPhp, SiMysql , SiFlask} from 'react-icons/si';
 import { MdPeople, MdOutlineChat } from 'react-icons/md'; 
 
 function Skills() {
@@ -22,13 +22,13 @@ function Skills() {
   ];
 
   const backendSkills = [
-    { name: 'php', icon: <SiPhp className="text-indigo-800" /> },
-    { name: 'Wordpress', icon: <FaWordpress className="text-blue-200" /> },
-    { name: 'Express.js', icon: <SiExpress className="text-gray-500" /> },
+    { name: 'Flask', icon: <SiFlask className="text-grey-200" /> },
+    { name: 'WordPress', icon: <FaWordpress className="text-blue-200" /> },
+    { name: 'Express.js', icon: <SiExpress className="text-grey-500" /> },
   ];
 
   const mlSkills=[
-    { name: 'PythonFlask', icon: <FaPython className="text-blue-400" /> },
+    { name: 'Python', icon: <FaPython className="text-blue-400" /> },
     { name: 'Pandas', icon: <SiPandas className="text-blue-800" /> },
     { name: 'Tensorflow', icon: <SiTensorflow className="text-yellow-800" /> },
   ];
